6e3c9cede303c9aa1882bc0c58c3f443cc43a244
Eric <eric.antoine@epitech.eu>
2018-12-03 21:10:31 +0100

    add resquest

--- index.php
+++ index.php
@@ -1,27 +1,25 @@
 <?php
     include("fonction/connect.php");
     $search_bar = $_GET["search_bar"];
-    //$query = $db_connect->query("Select titre from film");
-    $query = $db_connect->prepare("Select titre from film");
+    $query = $db_connect->prepare("SELECT titre FROM film WHERE titre LIKE '%". $search_bar ."%'");
     $query->execute();
     $array = $query -> fetchAll(PDO::FETCH_COLUMN);
     //echo $array[0];
 
     for($i  = 0; $i < count($array); $i++)
     {
-        if(strstr($search_bar, $array[$i]))
-        {
-            echo "$array[$i] \n";
-            //echo $search_bar;
-        }
+        echo "<div class='search'>$array[$i]</div><br/>";
     }
+
 ?>
+
+
 <html>
     <head></head>
-<body>
-    <form method="get" name="form" action="">
-        <input type="text" name="search_bar" value="Pretty Woman"></input>
-        <input type="submit">
-    </form>
-</body> 
+    <body>
+        <form method="get" name="form" action="">
+            <input type="text" name="search_bar" value="Pretty Woman"></input>
+            <input type="submit">
+        </form>
+</body>
 </html>
