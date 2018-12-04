473b58236e99cdc5fd8ab1d64bef01fda85fa55b
Eric <eric.antoine@epitech.eu>
2018-12-04 13:21:30 +0100

    add filter by genre

--- cinema.php
+++ cinema.php
@@ -22,8 +22,136 @@
                                     </div>
                                     <form method="get" name="form" action="">
                                         <div class="form-label-group">
-                                            <input type="text" name="search_bar" class="form-control" value="Pretty Woman"></input>
+                                            <label>Search Movies :</label>
+                                            <input type="text" name="search_bar" class="form-control"></input>
                                         </div>
+                                        <div class="form-label-group">
+                                            <label>Search Genre :</label>
+                                            <select class="form-control" id="sel1" name="genre" size="1">
+                                                <option>All</option>
+                                                <option>Action</option>
+                                                <option>Adventure</option>
+                                                <option>Animation</option>
+                                                <option>Biography</option>
+                                                <option>Comedy</option>
+                                                <option>Detective</option>
+                                                <option>Documentary</option>
+                                                <option>Drama</option>
+                                                <option>dramatic comedy</option>
+                                                <option>Erotic</option>
+                                                <option>Experimental</option>
+                                                <option>Family</option>
+                                                <option>Fantasy</option>
+                                                <option>Historical</option>
+                                                <option>Historical epic</option>
+                                                <option>Horror</option>
+                                                <option>Karate</option>
+                                                <option>Music</option>
+                                                <option>Musical</option>
+                                                <option>Program</option>
+                                                <option>Romance</option>
+                                                <option>Science fiction</option>
+                                                <option>Short film</option>
+                                                <option>Spying</option>
+                                                <option>Thriller</option>
+                                                <option>Unknow</option>
+                                                <option>Various</option>
+                                                <option>War</option>
+                                                <option>Western</option>
+                                            </select>
+                                        </div>
+                                        <div class="form-label-group">
+                                            <label>Search Distributeur :</label>
+                                            <select class="form-control" id="sel1" name="genre" size="1">
+                                            <option selected>All</option>
+                                                <option >20th century fox</option>
+                                                <option >Ace films</option>
+                                                <option >Adlabs films</option>
+                                                <option >Advanced</option>
+                                                <option >Art-light productions</option>
+                                                <option >Artificial eye</option>
+                                                <option >Axiom films</option>
+                                                <option >Bandidos films</option>
+                                                <option >Bfi distribution</option>
+                                                <option >Buena vista international</option>
+                                                <option >Buena vista international uk</option>
+                                                <option >Cerito films</option>
+                                                <option >Cinefilm</option>
+                                                <option >Contender entertainment</option>
+                                                <option >Davis-panzer productions</option>
+                                                <option >Delanic films</option>
+                                                <option >Deluxe productions</option>
+                                                <option >Diffusion pictures</option>
+                                                <option >Dmvb films</option>
+                                                <option >Dog eat dog productions</option>
+                                                <option >Dogwoof pictures</option>
+                                                <option >Eiffel productions</option>
+                                                <option >Entertainment film distributors ltd</option>
+                                                <option >Eros film ltd.</option>
+                                                <option >Eros international ltd.</option>
+                                                <option >Films a2</option>
+                                                <option >France 3 cin&amp;atilde;&amp;copy;ma</option>
+                                                <option >Gimages</option>
+                                                <option >Guerilla films ltd.</option>
+                                                <option >Halcyon pictures</option>
+                                                <option >Ica films</option>
+                                                <option >Icon film distribution uk</option>
+                                                <option >Idea productions</option>
+                                                <option >Interlight</option>
+                                                <option >Lagoon entertainment</option>
+                                                <option >Les films balenciaga</option>
+                                                <option >Les films du losange</option>
+                                                <option >Liberation entertainment</option>
+                                                <option >Lions gate films home entertainment</option>
+                                                <option >Lionsgate uk</option>
+                                                <option >Lolistar</option>
+                                                <option >Maiden voyage pictures</option>
+                                                <option >Metrodome films</option>
+                                                <option >Miracle comms</option>
+                                                <option >Mk2 diffusion</option>
+                                                <option >Momentum pictures</option>
+                                                <option >Monarchy enterprises b.v.</option>
+                                                <option >National film theater</option>
+                                                <option >Odeon sky filmworks</option>
+                                                <option >Optimum releasing</option>
+                                                <option >Paramount pictures</option>
+                                                <option >Paramount pictures uk</option>
+                                                <option >Parco co, ltd</option>
+                                                <option >Park circus</option>
+                                                <option >Peccadillo pictures</option>
+                                                <option >Punk distribution</option>
+                                                <option >Revelation films</option>
+                                                <option >Revolver entertainment</option>
+                                                <option >Rezo films</option>
+                                                <option >Showbox media group</option>
+                                                <option >Slingshot</option>
+                                                <option >Soda pictures</option>
+                                                <option >Sony pictures</option>
+                                                <option >Sony pictures uk</option>
+                                                <option >Spark pictures</option>
+                                                <option >Studio 18</option>
+                                                <option >Studio images 5</option>
+                                                <option >Swipe films</option>
+                                                <option >Tartan films</option>
+                                                <option >Telinor</option>
+                                                <option >The carousel pictures company</option>
+                                                <option >The vista organisation group</option>
+                                                <option >The works</option>
+                                                <option >Transfilm</option>
+                                                <option >Transmedia pictures</option>
+                                                <option >United international pictures (uip)</option>
+                                                <option >United pictures international uk</option>
+                                                <option >Universal</option>
+                                                <option >Universal international pictures</option>
+                                                <option >Utv motion pictures</option>
+                                                <option >Vertigo films</option>
+                                                <option >Verve pictures</option>
+                                                <option >Vision international</option>
+                                                <option >Warner bros u.k.</option>
+                                                <option >Warner music entertainment</option>
+                                                <option >Yeah yeah yeah ltd.</option>
+                                                <option >Yume pictures</option>
+                                            </select>
                                         <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Search</button>
                                     </form>
                                 </div>
@@ -37,15 +165,33 @@
 
 <?php
     include("fonction/connect.php");
-    $search_bar = $_GET["search_bar"];
-    $query = $db_connect->prepare("SELECT titre FROM film WHERE titre LIKE '%". $search_bar ."%'");
-    $query->execute();
-    $array = $query -> fetchAll(PDO::FETCH_COLUMN);
-    //echo $array[0];
 
-    for($i  = 0; $i < count($array); $i++)
+
+    function resquestFetch($resquest)
     {
-        echo "<div class='search'>$array[$i]</div><br/>";
+        global $db_connect;
+
+        $query = $db_connect->prepare($resquest);
+        $query->execute();
+        $array = $query->fetchAll(PDO::FETCH_ASSOC);
+
+        if($search_bar = $_GET["search_bar"])
+        {
+            for($i = 0; $i < count($array); $i++)
+            {
+                echo "<div class='search'>";
+                echo "<strong>Titre : </strong>" .$array[$i]["titre"] . "<br/>";
+                echo "<strong>Genre : </strong>" . $array[$i]["nom"] . "<br/>";
+                echo "<strong>Resum : </strong>" . $array[$i]["resum"] . "<br/>";
+                echo "</div><br/>";
+            } 
+        }
     }
 
+    if(isset($_GET["search_bar"]))
+    {
+        $search_bar = $_GET["search_bar"];    
+        //resquestFetch("SELECT titre,resum,date_debut_affiche FROM film WHERE titre LIKE '%". $search_bar ."%'");
+        resquestFetch("SELECT film.titre, genre.nom, film.resum FROM film LEFT JOIN genre ON film.id_genre = genre.nom WHERE titre LIKE '%". $search_bar ."%'");
+    }
 ?>
